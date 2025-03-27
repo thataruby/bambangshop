8226d18fe39fca5b2c355e40236405959d407e31
thataruby <thata@dfi.id>
2025-03-27 17:19:20 +0700

    Implement delete function in Subscriber repository.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -12,21 +12,33 @@ pub struct SubscriberRepository;
 impl SubscriberRepository{
     pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
         let subscriber_value = subscriber.clone();
-       if SUBSCRIBERS.get(product_type).is_none() {
+        if SUBSCRIBERS.get(product_type).is_none() {
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
-       };
+        };
 
-       SUBSCRIBERS.get(product_type).unwrap()
+        SUBSCRIBERS.get(product_type).unwrap()
            .insert(subscriber_value.url.clone(), subscriber_value);
-       return subscriber;
+        return subscriber;
    }
 
-   pub fn list_all(product_type: &str) -> Vec<Subscriber> {
-    if SUBSCRIBERS.get(product_type).is_none() {
-         SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
-    };
+    pub fn list_all(product_type: &str) -> Vec<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        };
 
-    return SUBSCRIBERS.get(product_type).unwrap().iter()
-        .map(|f| f.value().clone()).collect();
-}
+        return SUBSCRIBERS.get(product_type).unwrap().iter()
+            .map(|f| f.value().clone()).collect();
+    }
+
+    pub fn delete(product_type: &str, url: &str) -> Option<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        }
+        let result = SUBSCRIBERS.get(product_type).unwrap()
+            .remove(url);
+        if !result.is_none() {
+            return Some(result.unwrap().1);
+        }
+        return None;
+    }
 }
